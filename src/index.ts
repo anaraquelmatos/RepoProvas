8353f33a70f0fe3eeef5ac3ee4fc9e302d07b566
Ana Raquel Matos <anaraquelpmatos@gmail.com>
2022-07-20 05:27:26 -0300

    add: initial setup

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { json } from "express";
+import "express-async-errors";
+import dotenv from "dotenv";
+import errorHandler from "./middlewares/errorHandlerMiddleware.js";
+import router from "./routers/index.js";
+dotenv.config();
+
+const App = express();
+
+App.use(json());
+App.use(router);
+App.use(errorHandler);
+
+const port = +process.env.PORT || 4000;
+
+App.listen(port, () => {
+    console.log(`Server up and running on port ${port}`);
+})
---
+++ src/middlewares/errorHandlerMiddleware.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+
+export default function errorHandler(error, req: Request, res: Response, next: NextFunction) {
+
+    if (error.type === "unauthorized") {
+        return res.status(401).send({ message: error.message });
+    }
+    if (error.type === "not found") {
+        return res.status(404).send({ message: error.message });
+    }
+    if (error.type === "conflict") {
+        return res.status(409).send({ message: error.message });
+    }
+    if (error.response) {
+        return res.sendStatus(error.response.status);
+    }
+
+    console.log(error);
+
+    res.sendStatus(500);
+}
